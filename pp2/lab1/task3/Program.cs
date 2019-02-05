9c250db3cae148575431fe7a190b8255ef21e3ae
shaded92 <damir.shaltaev@gmail.com>
2019-02-05 12:57:00 +0600

    task3 commit

--- pp2/lab1/task3/Program.cs
+++ pp2/lab1/task3/Program.cs
@@ -10,6 +10,20 @@ namespace task3
     {
         static void Main(string[] args)
         {
+            int x = int.Parse(Console.ReadLine());  //getting the "size" of input
+            int[] a = new int[x];                   // array to store inputs
+            for (int i = 0; i < x; ++i)
+            {
+                a[i] = int.Parse(Console.ReadLine());   // getting the array itself
+            }
+
+                for (int i = 0; i < a.Length; ++i)
+            {
+                for (int j = 0; j < 2; ++j)             //loop to twice every output
+                {
+                    Console.Write(a[i] + " ");
+                }
+            }
         }
     }
 }
