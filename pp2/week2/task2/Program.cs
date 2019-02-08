e9b262fbe8f44815a90cde82f8d2a3cfdc0ed997
shaded92 <damir.shaltaev@gmail.com>
2019-02-09 00:00:30 +0600

    task2

---
+++ pp2/week2/task2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+    class Program
+    {
+        public static bool Prime(int c)
+        {
+            if (c == 1)
+                return false;
+            for (int i = 2; i < c; i++)
+            {
+                if (c % i == 0) return false;
+            }
+            return true;
+        }
+        static string Solve(string str)
+        {
+            string[] x = str.Split(' ');
+            string Pnum = null;
+
+            for (int i = 0; i < x.Length; ++i)
+            {
+                if (Prime(int.Parse(x[i])))
+                {
+                    Pnum = Pnum + " " + x[i];
+
+                }
+            }
+            return Pnum;
+        }
+        static void Main(string[] args)
+        {
+            FileStream fs = new FileStream(@"C:\test\innerTest\hello.txt", FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+
+            string line = sr.ReadLine();
+
+            File.WriteAllText(@"C:\test\innerTest\output.txt", Solve(line));
+
+
+            sr.Close();
+            fs.Close();
+        }
+    }
+}
