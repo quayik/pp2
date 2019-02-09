eb47c06a4bb1bcad64ad1f497de2768ca8dbb5de
shaded92 <damir.shaltaev@gmail.com>
2019-02-09 19:52:14 +0600

    new commit

---
+++ pp2/week2/task4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path1 = @"C:\test\1\text.txt";
+            string path2 = @"C:\test\1\1 2\text2.txt";
+            File.Move(path1, path2);
+        }
+    }
+}
