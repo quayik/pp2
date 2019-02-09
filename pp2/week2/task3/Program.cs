fbb8974e1994aa76162d5793153eae28df1bdd51
shaded92 <damir.shaltaev@gmail.com>
2019-02-09 19:22:52 +0600

    new commit

--- pp2/week2/task3/Program.cs
+++ pp2/week2/task3/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,41 @@ namespace task3
 {
     class Program
     {
+        static int cnt = 0;
+        public FileSystemInfo[] Items
+        {
+            get;
+            set;
+        }
+
+        public static void Out(FileSystemInfo[] fsi)
+        {
+            for (int i = 0; i < fsi.Length; ++i)
+            {
+                for (int j = 0; j < cnt; ++j)
+                {
+                    Console.Write("\t");
+                }
+
+                if (fsi[i].GetType() == typeof(DirectoryInfo))
+                {
+                    Console.WriteLine(fsi[i].Name);
+                    DirectoryInfo d = fsi[i] as DirectoryInfo;
+                    cnt++;
+                    Out(d.GetFileSystemInfos());
+                }
+                else
+                {
+                    Console.WriteLine(fsi[i].Name);
+                }
+                if (i == fsi.Length - 1) cnt--;
+            }
+        }
+
         static void Main(string[] args)
         {
+            DirectoryInfo dir = new DirectoryInfo(@"C:\test");
+            Out(dir.GetFileSystemInfos());
         }
     }
 }
