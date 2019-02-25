e1fb49d8804afe6a77ae565680fb5974def99e25
shaded92 <damir.shaltaev@gmail.com>
2019-02-25 10:11:43 +0600

    added task1'

---
+++ pp2/week5/task1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Example2
+{
+    public class ComplexNumber
+    {
+        public double a;
+        public double b;
+        public ComplexNumber()
+        {
+            
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            F1();
+        }
+
+        private static void F2()
+        {
+            FileStream fs = new FileStream("cn.txt", FileMode.Open, FileAccess.Read);
+            XmlSerializer xs = new XmlSerializer(typeof(ComplexNumber));
+
+            ComplexNumber t = xs.Deserialize(fs) as ComplexNumber;
+
+            fs.Close();
+        }
+
+        private static void F1()
+        {
+            ComplexNumber cn = new ComplexNumber
+            {
+                a = 3,
+                b = 2.4
+            };
+           
+
+            FileStream fs = new FileStream("cn.txt", FileMode.Create, FileAccess.Write);
+            XmlSerializer xs = new XmlSerializer(typeof(ComplexNumber));
+
+            xs.Serialize(fs, cn);
+
+            fs.Close();
+        }
+    }
+}
